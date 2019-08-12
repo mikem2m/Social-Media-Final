2212075fe53c0c7af49368fa005b4826c4056203
mikem2m <michael08ace@gmail.com>
2019-08-12 09:27:11 +0800

    Mobile Commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -19,8 +19,11 @@ const reducers = combineReducers({
 const store = createStore(
     reducers,
     initialState,
-    compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+    applyMiddleware(...middleware)
+);
+
+    // compose(
+    //     applyMiddleware(...middleware),
+    //     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
 
 export default store;
